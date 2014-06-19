7d60f042faf96300e8ae9158944e3ea0f4b844a4
John Victor-Faichney <john.victor-faichney@aditazz.com>
2014-06-18 22:41:02 -0700

    added out-of-box TrackBall and tooltips

--- webgl/public/js/controllers.js
+++ webgl/public/js/controllers.js
@@ -2,59 +2,69 @@
 function scene_controller() {
 
 	var self = this;
-
-	var scene,
+    var container;
+    var tooltip;
+    
+	var controls,
+    scene,
 	camera,
 	renderer,
+    projector,
 	geometry,
 	material,
 	cube;
     
     var tracing_template;
-
-	// **** Utility methods ****
+    var objects = [],plane;
     
-    // Set the default value for all variables
+    var mouse = new THREE.Vector2(),
+    offset = new THREE.Vector3(),
+    INTERSECTED, SELECTED;
+    
+    // Initializes threeJS stuff
 	var __construct = function (that) {
-
+    
 		scene = new THREE.Scene();
 		camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 		renderer = new THREE.WebGLRenderer();
-
+        projector = new THREE.Projector();
+        
+        container = document.createElement('div');
+        
+        document.body.appendChild(container);
+    
 		renderer.setSize(window.innerWidth, window.innerHeight);
-
-		document.body.appendChild(renderer.domElement);
-
-		geometry = new THREE.CubeGeometry(1, 1, 1);
-		material = new THREE.MeshBasicMaterial({
-				color : 0x00ff00
-			});
-		cube = new THREE.Mesh(geometry, material);
-
-		scene.add(cube);
-
+		container.appendChild(renderer.domElement);
 		camera.position.z = 100;
+        
+        
+        controls = new THREE.TrackballControls(camera, container);
+        controls.rotateSpeed = 1.0;
+        controls.zoomSpeed = 1.2;
+        controls.panSpeed = 0.8;
+        controls.noZoom = false;
+        controls.noPan = false;
+        controls.staticMoving = true;
+        controls.dynamicDampingFactor = 0.3;
+        
+        
+        plane = new THREE.Mesh(new THREE.PlaneGeometry(2000, 2000, 8, 8), new THREE.MeshBasicMaterial({
+                    color : 0x000000,
+                    opacity : 0.25,
+                    transparent : true,
+                    wireframe : true
+                }));
+        plane.visible = false;
+        scene.add(plane);
+    
+        renderer.domElement.addEventListener('mousemove', onDocumentMouseMove, false);
+        renderer.domElement.addEventListener('mousedown', onDocumentMouseDown, false);
+        renderer.domElement.addEventListener('mouseup', onDocumentMouseUp, false);
 
+        window.addEventListener('resize', onWindowResize, false);
 	}(this);
     
-    // Clear the scene geometry
-	self.clear_scene = function () {
-		console.log("clear_scene();");
-	};
-    
-
-    // Render the scene
-	self.render = function () {
-		requestAnimationFrame(self.render);
-
-		// cube.rotation.x += 0.01;
-		// cube.rotation.y += 0.01;
-
-		renderer.render(scene, camera);
-	};
-
-	// Visualization methods
-    // Changes the canvas (and redraws geometry) according to the selected frame
+    // TBD: Changes the active frame
 	self.set_frame = function (frame_name) {
     
 		console.log("set_frame("+frame_name+")");
@@ -77,7 +87,7 @@ function scene_controller() {
         
 	};
     
-    // Changes the active template.
+    // Changes the active tracing template.
 	self.set_tracing_template = function (tracing_template_name) {
     
          // Each template has a constructor function called template_constructor
@@ -86,26 +96,160 @@ function scene_controller() {
         });
 	};
     
-    // adds the appropriate geometry  when a json is received from db_controller
-    
-    // Tracings are created by combining a set of data with a corresponding 
-    // tracing template (which receives each data element and correctly generates the 
-    // corresponding threeJS geometry).  These templates are separate files loaded in as needed.
-    // setting them procedurally will allow the same data to produce many different geometrical representations
-    // and allow new types of data to be easily added/removed
+    // Adds a tracing to the scene by combining a set of data with a corresponding tracing template
 	self.add_tracing = function (json) {
         json.data.forEach(function(trace_json){
             trace_properties = trace_json[0]["data"];
             trace = tracing_template(trace_properties);
+            objects.push(trace);
             console.log(trace);
             scene.add(trace);
         });
-        self.render();
+        self.animate();
+	};
+    
+    // **** Helper Functions***
+        
+    // Clear the scene geometry
+	self.clear_scene = function () {
+        objects.forEach(function(object){
+            scene.remove(object);
+        });
 	};
 
+    // Animate the scene
+	self.animate = function () {
+        controls.update();
+		requestAnimationFrame(self.animate);
+        
+         objects.forEach(function(object){
+            object.animate();
+        });
+
+		renderer.render(scene, camera);
+	};
+    
+    self.display_tooltip = function(text, location){
+        tooltip = document.createElement('div');
+        tooltip.style.position = 'absolute';
+        //text2.style.zIndex = 1;    // if you still don't see the label, try uncommenting this
+        tooltip.style.color = "white";
+        tooltip.innerHTML = text;
+        tooltip.style.top = location.y + 'px';
+        tooltip.style.left = location.x + 'px';
+        document.body.appendChild(tooltip);
+    }
+    
+    self.hide_tooltip = function(){
+        if (tooltip != undefined){
+            document.body.removeChild(tooltip);
+            console.log("removed");
+        }
+    }
+    
+    // **** ThreeJS Helper Functions***
+    // BORROWED FROM: http://mrdoob.github.io/three.js/examples/webgl_interactive_draggablecubes.html
+    
+    function onWindowResize() {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+
+        renderer.setSize( window.innerWidth, window.innerHeight );
+    }
+    
+    function onDocumentMouseMove( event ) {
+        event.preventDefault();
+
+        mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+        mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+        var vector = new THREE.Vector3( mouse.x, mouse.y, 0.5 );
+        projector.unprojectVector( vector, camera );
+        var raycaster = new THREE.Raycaster( camera.position, vector.sub( camera.position ).normalize() );
+
+        if ( SELECTED ) {
+
+            var intersects = raycaster.intersectObject( plane );
+            SELECTED.position.copy( intersects[ 0 ].point.sub( offset ) );
+            return;
+        }
+        
+        var intersects = raycaster.intersectObjects( objects );
+
+        if ( intersects.length > 0 ) {
+            if ( INTERSECTED != intersects[ 0 ].object ) {
+                if ( INTERSECTED ) INTERSECTED.material.color.setHex( INTERSECTED.currentHex );
+
+                INTERSECTED = intersects[ 0 ].object;
+                INTERSECTED.currentHex = INTERSECTED.material.color.getHex();
+
+                plane.position.copy( INTERSECTED.position );
+                plane.lookAt( camera.position );
+            }
+
+            container.style.cursor = 'pointer';
+        } else {
+
+            if ( INTERSECTED ) INTERSECTED.material.color.setHex( INTERSECTED.currentHex );
+            INTERSECTED = null;
+            container.style.cursor = 'auto';
+        }
+    }
+
+    function onDocumentMouseDown( event ) {
+
+        event.preventDefault();
+
+        var vector = new THREE.Vector3( mouse.x, mouse.y, 0.5 );
+        projector.unprojectVector( vector, camera );
+
+        var raycaster = new THREE.Raycaster( camera.position, vector.sub( camera.position ).normalize() );
+
+        var intersects = raycaster.intersectObjects( objects );
+
+        if ( intersects.length > 0 ) {
+            controls.enabled = false;
+            SELECTED = intersects[ 0 ].object;
+            var intersects = raycaster.intersectObject( plane );
+            offset.copy( intersects[ 0 ].point ).sub( plane.position );
+            container.style.cursor = 'move';
+        }
+        
+    }
+
+    function onDocumentMouseUp( event ) {
+
+        event.preventDefault();
+        controls.enabled = true;
+
+        if ( INTERSECTED ) {
+            plane.position.copy( INTERSECTED.position );
+            SELECTED = null;
+            
+            // Find location on canvas and show tweet as overlay
+            pos = toXYCoords(INTERSECTED.position);
+            text = INTERSECTED.get_metadata();
+            console.log(pos);
+            console.log( text);
+            self.hide_tooltip();
+            self.display_tooltip(text, pos);
+        }
+
+        container.style.cursor = 'auto';
+    }
+    
+    // Projects 3D position to x,y position on canvas
+    var toXYCoords = function (pos) {
+        var vector = projector.projectVector(pos.clone(), camera);
+        vector.x = (vector.x + 1)/2 * window.innerWidth;
+        vector.y = -(vector.y - 1)/2 * window.innerHeight;
+        return vector;
+    }
+    
 }
-
-
+ 
+ 
+ 
 // Class for making calls to Neo4j/Postgres
 function db_controller() {
     var self = this;
--- webgl/public/tracing_templates/tweet_node.js
+++ webgl/public/tracing_templates/tweet_node.js
@@ -1,11 +1,11 @@
 var template_constructor = function(trace_json){
-
-     console.log( trace_json);
-     
+    var self = this;
+    
+     self.text = trace_json.text;
      var x = (trace_json.lat-22.28)*1000;
      var y = (trace_json.lon-114.15)*1000;
      
-     console.log("x: "+x+",y: "+y);
+    console.log("x: "+x+",y: "+y);
     geometry = new THREE.CubeGeometry(8, 8, 8);
     material = new THREE.MeshBasicMaterial({
             color : 0x00ff00,
@@ -14,7 +14,15 @@ var template_constructor = function(trace_json){
         
     cube = new THREE.Mesh(geometry, material);
     cube.position.set(x, y, 0);
-    console.log( "created trace!");
     
+    cube.animate = function(){
+        this.rotation.x += 0.001;
+		this.rotation.y += 0.003;
+    }
+    
+    cube.get_metadata = function(){
+        return  trace_json.text;
+    }
+
     return cube
 }
